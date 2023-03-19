7934996c2e8d5f6bdf608ad02a1bf67301197a20
Fortune-Ndlovu <109510679+Fortune-Ndlovu@users.noreply.github.com>
2023-03-19 00:28:37 +0000

    mapped thrugh an an array within an object and displayed the datat in paragraph for each piece of data

---
+++ src/components/Course.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Course;
