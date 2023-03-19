893f709cc2515c29d135e8723ee64aed3d2d4fb5
Fortune-Ndlovu <109510679+Fortune-Ndlovu@users.noreply.github.com>
2023-03-19 11:58:14 +0000

    Added an state to control a new input field we have also added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", id: 1, userNumber: "555-555-555" }
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (e) => {
     e.preventDefault();
@@ -14,13 +17,18 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
   const addPerson = (e) => {
     e.preventDefault();
     const personObject = {
       name: newName,
-      id: persons.length + 1
+      id: persons.length + 1,
+      userNumber: newNumber
     };
-
+    console.log(personObject);
     let personExits = false;
     for (let i = 0; i < persons.length; i++) {
       if (personObject.name === persons[i].name) {
@@ -30,11 +38,11 @@ const App = () => {
     }
     if (personExits) {
       alert(`${personObject.name} has already been added`);
-    } else{
+    } else {
       setPersons(persons.concat(personObject));
       setNewName("");
+      setNewNumber("");
     }
-    
   };
 
   return (
@@ -45,13 +53,18 @@ const App = () => {
           name: <input type={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input type={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <p key={person.id}>{person.name}</p>
+          <p key={person.id}>
+            {person.name} {person.userNumber}
+          </p>
         ))}
       </div>
     </div>
