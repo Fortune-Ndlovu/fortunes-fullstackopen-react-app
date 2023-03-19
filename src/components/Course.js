c4ca361250e93949236bde4a6460302d55df3aa1
Fortune-Ndlovu <109510679+Fortune-Ndlovu@users.noreply.github.com>
2023-03-19 02:21:20 +0000

    Added more data to our array and mapped its indiviual indexs

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,23 +1,39 @@
 import React from "react";
 
 const Course = ({ course }) => {
-  let courseExercises = course.parts.map((item) => item.exercises);
+  let courseExercises = course[0].parts.map((item) => item.exercises);
   let initialValue = 0;
   let sumWithInitialValue = courseExercises.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     initialValue
   );
 
+  let courseExercisesIndex2 = course[1].parts.map((item) => item.exercises);
+  let initialValueIndex2 = 0;
+  let sumWithInitialValueIndex2 = courseExercisesIndex2.reduce(
+    (accumulatorIndex2, currentValueIndex2) =>
+      accumulatorIndex2 + currentValueIndex2,
+    initialValueIndex2
+  );
+
   return (
     <div>
-      <h1>{course.name}</h1>
-      {course.parts.map((item) => (
+      <h1>{course[0].name}</h1>
+      {course[0].parts.map((item) => (
         <p key={item.id}>
           {item.name}
           {item.exercises}
         </p>
       ))}
-      <b>Total exercises: {sumWithInitialValue}</b>
+      <strong>Total exercises: {sumWithInitialValue}</strong>
+
+      <p><b>{course[1].name}</b></p>    
+      {course[1].parts.map((item) => (
+        <p key={item.id}>
+          {item.name} {item.exercises}
+        </p>
+      ))}
+      <strong>Total exercises: {sumWithInitialValueIndex2}</strong>
     </div>
   );
 };
