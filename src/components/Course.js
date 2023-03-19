d6a7ec723440f295180c4e04c00be001f770d0f6
Fortune-Ndlovu <109510679+Fortune-Ndlovu@users.noreply.github.com>
2023-03-19 02:33:38 +0000

    we used the spread operator to create a spnaking new array

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 const Course = ({ course }) => {
-  let courseExercises = course[0].parts.map((item) => item.exercises);
+  let tempCourseArr = [...course];
+
+  let courseExercises = tempCourseArr[0].parts.map((item) => item.exercises);
   let initialValue = 0;
   let sumWithInitialValue = courseExercises.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     initialValue
   );
 
-  let courseExercisesIndex2 = course[1].parts.map((item) => item.exercises);
+  let courseExercisesIndex2 = tempCourseArr[1].parts.map(
+    (item) => item.exercises
+  );
   let initialValueIndex2 = 0;
   let sumWithInitialValueIndex2 = courseExercisesIndex2.reduce(
     (accumulatorIndex2, currentValueIndex2) =>
@@ -18,8 +22,8 @@ const Course = ({ course }) => {
 
   return (
     <div>
-      <h1>{course[0].name}</h1>
-      {course[0].parts.map((item) => (
+      <h1>{tempCourseArr[0].name}</h1>
+      {tempCourseArr[0].parts.map((item) => (
         <p key={item.id}>
           {item.name}
           {item.exercises}
@@ -27,8 +31,10 @@ const Course = ({ course }) => {
       ))}
       <strong>Total exercises: {sumWithInitialValue}</strong>
 
-      <p><b>{course[1].name}</b></p>    
-      {course[1].parts.map((item) => (
+      <p>
+        <b>{tempCourseArr[1].name}</b>
+      </p>
+      {tempCourseArr[1].parts.map((item) => (
         <p key={item.id}>
           {item.name} {item.exercises}
         </p>
